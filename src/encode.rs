0904270765287d26d2a73aecb50112662389b3b8
Alex Hamlin <alex@alexhamlin.co>
2022-11-05 16:44:33 -0700

    Comments and cleanup

--- src/encode.rs
+++ src/encode.rs
@@ -42,16 +42,19 @@ where
     fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
         let mut written = 0;
 
+        // First, emit the remainder of any character that a previous read could
+        // not fully emit.
         if !self.remainder.get_ref().is_empty() {
             let len = std::cmp::min(buf.len(), self.remainder.get_ref().len());
             self.remainder.read_exact(&mut buf[..len])?;
             buf = &mut buf[len..];
             written += len;
-            if !self.remainder.get_ref().is_empty() {
+            if buf.is_empty() {
                 return Ok(written);
             }
         }
 
+        // Second, emit as much as we can directly into the destination buffer.
         while buf.len() >= MAX_UTF8_ENCODED_LEN {
             let ch = match self.read_next_char()? {
                 Some(ch) => ch,
@@ -62,6 +65,9 @@ where
             written += len;
         }
 
+        // Finally, emit as much as we can into the destination buffer's
+        // remaining space, storing the remainder of any character that we
+        // cannot fully emit at this time.
         while !buf.is_empty() {
             let ch = match self.read_next_char()? {
                 Some(ch) => ch,
@@ -75,8 +81,7 @@ where
             buf[..emit_len].copy_from_slice(&tmp[..emit_len]);
             buf = &mut buf[emit_len..];
             written += emit_len;
-
-            if emit_len < char_len {
+            if buf.is_empty() {
                 self.remainder.write_all(&tmp[emit_len..char_len])?;
             }
         }
