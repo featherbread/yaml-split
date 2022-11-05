4a1ce8d5a13815c5752f7f2f0e273d7a9f320d02
Alex Hamlin <alex@alexhamlin.co>
2022-11-05 15:07:41 -0700

    A bit of a different direction

--- src/split.rs
+++ src/split.rs
@@ -1,26 +1,24 @@
-use std::io::{self, Read};
+use std::io::{self, BufRead, Read};
 use std::mem::MaybeUninit;
 use std::pin::Pin;
 
 use unsafe_libyaml::*; // It's called a proof-of-concept, okay?
 
-const MAX_UTF8_CHAR_BYTES: usize = 4;
-
-struct CharBuffer {
-    buf: [u8; MAX_UTF8_CHAR_BYTES],
+struct SmallBuffer<const SIZE: usize> {
+    buf: [u8; SIZE],
     len: usize,
 }
 
-impl Default for CharBuffer {
+impl<const SIZE: usize> Default for SmallBuffer<SIZE> {
     fn default() -> Self {
         Self {
-            buf: [0u8; MAX_UTF8_CHAR_BYTES],
+            buf: [0u8; SIZE],
             len: 0,
         }
     }
 }
 
-impl CharBuffer {
+impl<const SIZE: usize> SmallBuffer<SIZE> {
     fn new() -> Self {
         Self::default()
     }
@@ -29,14 +27,14 @@ impl CharBuffer {
         self.len == 0
     }
 
-    fn put(&mut self, c: char) {
+    fn overwrite_char_utf8(&mut self, c: char) {
         let s = c.encode_utf8(&mut self.buf);
-        debug_assert!(s.len() <= MAX_UTF8_CHAR_BYTES);
+        assert!(s.len() <= SIZE);
         self.len = s.len();
     }
 }
 
-impl Read for CharBuffer {
+impl<const SIZE: usize> Read for SmallBuffer<SIZE> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let len = std::cmp::min(buf.len(), self.len);
         (&mut buf[..len]).clone_from_slice(&self.buf[..len]);
@@ -46,34 +44,70 @@ impl Read for CharBuffer {
     }
 }
 
-enum YAMLEncoding {
-    UTF8,
-    UTF16BE,
-    UTF16LE,
-    UTF32BE,
-    UTF32LE,
-}
+const YAML_ENCODING_PREFIX_LEN: usize = 4;
+const MAX_UTF8_CHAR_BYTES: usize = 4;
 
 struct YAMLStreamEncoder<R>
 where
     R: Read,
 {
     source: R,
-    encoding: Option<YAMLEncoding>,
-    input_buf: Vec<u8>,
-    output_buf: CharBuffer,
+    detect_prefix: SmallBuffer<YAML_ENCODING_PREFIX_LEN>,
+    output_prefix: SmallBuffer<MAX_UTF8_CHAR_BYTES>,
 }
 
-impl<R> YAMLStreamEncoder<R>
+enum UTF32Endianness {
+    BE,
+    LE,
+}
+
+impl UTF32Endianness {
+    fn decode(&self, buf: [u8; 4]) -> u32 {
+        match self {
+            Self::BE => u32::from_be_bytes(buf),
+            Self::LE => u32::from_le_bytes(buf),
+        }
+    }
+}
+
+struct UTF32StreamEncoder<R>
 where
-    R: Read,
+    R: BufRead,
 {
-    fn new(source: R) -> Self {
-        Self {
-            source,
-            input_buf: Vec::new(),
-            encoding: None,
-            output_buf: CharBuffer::default(),
+    source: R,
+    endianness: UTF32Endianness,
+}
+
+impl<R> UTF32StreamEncoder<R>
+where
+    R: BufRead,
+{
+    fn new(source: R, endianness: UTF32Endianness) -> Self {
+        Self { source, endianness }
+    }
+}
+
+impl<R> Iterator for UTF32StreamEncoder<R>
+where
+    R: BufRead,
+{
+    type Item = Result<char, io::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.source.fill_buf() {
+            Ok(buf) if buf.len() == 0 => return None,
+            Err(err) => return Some(Err(err)),
+            _ => {}
+        };
+
+        let mut buf = [0u8; 4];
+        if let Err(err) = self.source.read_exact(&mut buf) {
+            return Some(Err(err));
+        }
+
+        match char::from_u32(self.endianness.decode(buf.try_into().unwrap())) {
+            Some(ch) => Some(Ok(ch)),
+            None => Some(Err(io::ErrorKind::InvalidInput.into())),
         }
     }
 }
