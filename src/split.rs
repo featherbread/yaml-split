2073ca7a9e970ef88e075207b258818ba0495738
Alex Hamlin <alex@alexhamlin.co>
2022-11-05 14:13:15 -0700

    Start the skeleton for the streaming UTF-8 encoder

--- src/split.rs
+++ src/split.rs
@@ -1,7 +1,82 @@
+use std::io::{self, Read};
 use std::mem::MaybeUninit;
 use std::pin::Pin;
 
 use unsafe_libyaml::*; // It's called a proof-of-concept, okay?
+
+const MAX_UTF8_CHAR_BYTES: usize = 4;
+
+struct CharBuffer {
+    buf: [u8; MAX_UTF8_CHAR_BYTES],
+    len: usize,
+}
+
+impl Default for CharBuffer {
+    fn default() -> Self {
+        Self {
+            buf: [0u8; MAX_UTF8_CHAR_BYTES],
+            len: 0,
+        }
+    }
+}
+
+impl CharBuffer {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    fn put(&mut self, c: char) {
+        let s = c.encode_utf8(&mut self.buf);
+        debug_assert!(s.len() <= MAX_UTF8_CHAR_BYTES);
+        self.len = s.len();
+    }
+}
+
+impl Read for CharBuffer {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = std::cmp::min(buf.len(), self.len);
+        (&mut buf[..len]).clone_from_slice(&self.buf[..len]);
+        self.buf.copy_within(len.., 0);
+        self.len -= len;
+        return Ok(len);
+    }
+}
+
+enum YAMLEncoding {
+    UTF8,
+    UTF16BE,
+    UTF16LE,
+    UTF32BE,
+    UTF32LE,
+}
+
+struct YAMLStreamEncoder<R>
+where
+    R: Read,
+{
+    source: R,
+    encoding: Option<YAMLEncoding>,
+    input_buf: Vec<u8>,
+    output_buf: CharBuffer,
+}
+
+impl<R> YAMLStreamEncoder<R>
+where
+    R: Read,
+{
+    fn new(source: R) -> Self {
+        Self {
+            source,
+            input_buf: Vec::new(),
+            encoding: None,
+            output_buf: CharBuffer::default(),
+        }
+    }
+}
 
 pub struct Splitter {
     parser: Pin<Box<yaml_parser_t>>,
