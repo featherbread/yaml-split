efa4792981016fa5df4ed2a5f7144cb2ac86c05c
Alex Hamlin <alex@alexhamlin.co>
2022-11-05 18:16:27 -0700

    Implement a streaming UTF-16 decoder
    I ended up writing my own implementation based on the one from the
    standard library, which only works on iterators of u16s and therefore
    made it awkward to pass I/O errors through to the UTF-8 encoding side.
    (Honestly, I also think my version is a little easier to read.)

--- src/encode.rs
+++ src/encode.rs
@@ -86,16 +86,109 @@ where
 }
 
 pub enum Endianness {
-    BE,
-    LE,
+    Big,
+    Little,
 }
 
 impl Endianness {
+    fn decode_u16(&self, buf: [u8; 2]) -> u16 {
+        match self {
+            Endianness::Big => u16::from_be_bytes(buf),
+            Endianness::Little => u16::from_le_bytes(buf),
+        }
+    }
+
     fn decode_u32(&self, buf: [u8; 4]) -> u32 {
         match self {
-            Endianness::BE => u32::from_be_bytes(buf),
-            Endianness::LE => u32::from_le_bytes(buf),
+            Endianness::Big => u32::from_be_bytes(buf),
+            Endianness::Little => u32::from_le_bytes(buf),
         }
+    }
+}
+
+pub struct UTF16Decoder<R>
+where
+    R: BufRead,
+{
+    source: R,
+    endianness: Endianness,
+    buf: Option<u16>,
+}
+
+impl<R> UTF16Decoder<R>
+where
+    R: BufRead,
+{
+    pub fn new(source: R, endianness: Endianness) -> Self {
+        Self {
+            source,
+            endianness,
+            buf: None,
+        }
+    }
+}
+
+impl<R> Iterator for UTF16Decoder<R>
+where
+    R: BufRead,
+{
+    type Item = io::Result<char>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let lead = match self.buf.take() {
+            Some(u) => u,
+            None => match self.next_u16() {
+                Ok(Some(u)) => u,
+                Ok(None) => return None,
+                Err(err) => return Some(Err(err)),
+            },
+        };
+
+        if !(0xD800..=0xDFFF).contains(&lead) {
+            // SAFETY: This is not a UTF-16 surrogate, which means that the u16
+            // code unit directly encodes the desired code point.
+            return Some(Ok(unsafe { char::from_u32_unchecked(lead as u32) }));
+        }
+
+        if lead >= 0xDC00 {
+            // Invalid: a UTF-16 trailing surrogate with no leading surrogate.
+            return Some(Ok(char::REPLACEMENT_CHARACTER));
+        }
+
+        let trail = match self.next_u16() {
+            Ok(Some(u)) => u,
+            Ok(None) => return Some(Ok(char::REPLACEMENT_CHARACTER)),
+            Err(err) => return Some(Err(err)),
+        };
+        if !(0xDC00..=0xDFFF).contains(&trail) {
+            // Invalid: we needed a trailing surrogate and didn't get one. We'll
+            // try to decode this as a leading code unit on the next iteration.
+            self.buf = Some(trail);
+            return Some(Ok(char::REPLACEMENT_CHARACTER));
+        }
+
+        // At this point, we are confident that we have valid leading and
+        // trailing surrogates, and can decode them into the correct code point.
+        let ch = 0x1_0000 + (((lead - 0xD800) as u32) << 10 | (trail - 0xDC00) as u32);
+        // SAFETY: We have confirmed that the surrogate pair is valid.
+        Some(Ok(unsafe { char::from_u32_unchecked(ch as u32) }))
+    }
+}
+
+impl<R> UTF16Decoder<R>
+where
+    R: BufRead,
+{
+    fn next_u16(&mut self) -> io::Result<Option<u16>> {
+        match self.source.fill_buf() {
+            Ok(buf) if buf.is_empty() => return Ok(None),
+            Err(err) => return Err(err),
+            _ => {}
+        };
+
+        let mut buf = [0u8; 2];
+        self.source.read_exact(&mut buf)?;
+        Ok(Some(self.endianness.decode_u16(buf)))
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,43 @@ use std::io::{self, Read};
 use encode::{Endianness, UTF32Decoder, UTF8Encoder};
 use split::Splitter;
 
-static HELLO_UTF32BE: &[u8] = include_bytes!("hello.txt");
+use crate::encode::UTF16Decoder;
+
+static HELLO_UTF32BE: &[u8] = include_bytes!("hello-utf32be.txt");
+static HELLO_UTF16LE: &[u8] = include_bytes!("hello-utf16le.txt");
 
 fn main() {
     Splitter::new();
     println!("Splitter didn't crash!");
 
-    let encoder = UTF8Encoder::new(UTF32Decoder::new(HELLO_UTF32BE, Endianness::BE));
-    print!("{}", io::read_to_string(encoder).unwrap());
+    print_string(UTF8Encoder::new(UTF16Decoder::new(
+        HELLO_UTF16LE,
+        Endianness::Little,
+    )));
+    print_string(UTF8Encoder::new(UTF32Decoder::new(
+        HELLO_UTF32BE,
+        Endianness::Big,
+    )));
 
-    let encoder = UTF8Encoder::new(UTF32Decoder::new(HELLO_UTF32BE, Endianness::BE));
+    print_bytes_and_string(UTF8Encoder::new(UTF16Decoder::new(
+        HELLO_UTF16LE,
+        Endianness::Little,
+    )));
+    print_bytes_and_string(UTF8Encoder::new(UTF32Decoder::new(
+        HELLO_UTF32BE,
+        Endianness::Big,
+    )));
+}
+
+fn print_string(r: impl Read) {
+    print!("{}", io::read_to_string(r).unwrap());
+}
+
+fn print_bytes_and_string(r: impl Read) {
     print!(
         "{}",
         String::from_utf8(
-            encoder
-                .bytes()
+            r.bytes()
                 .map(|b| b.unwrap())
                 .inspect(|b| print!("{:?} ", b))
                 .collect(),
