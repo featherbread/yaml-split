88830959b9fc9fc575bf6eac36fab6eb7f2bcd1e
Alex Hamlin <alex@alexhamlin.co>
2022-11-05 21:25:23 -0700

    Set up a full iconv-like harness for the re-encoder
    It's about 3 times faster than iconv (in release mode) even though the
    code is pretty naïve. That should be more than good enough for my needs.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,65 @@
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter};
+use std::path::PathBuf;
+
+use clap::Parser;
+
 mod encode;
+#[allow(dead_code)]
 mod split;
 
-use std::io::{self, Read};
-
-use encode::Encoding;
-use split::Splitter;
-
-static HELLO_UTF32BE: &[u8] = include_bytes!("hello-utf32be.txt");
-static HELLO_UTF16LE: &[u8] = include_bytes!("hello-utf16le.txt");
+use encode::{Encoding, Endianness};
 
 fn main() {
-    {
-        Splitter::new();
+    let cli = Cli::parse();
+    let mut w = BufWriter::new(io::stdout());
+    let mut r = match cli.inputfile {
+        None => cli.from_code.utf8_reader(io::stdin().lock()),
+        Some(filename) => cli
+            .from_code
+            .utf8_reader(BufReader::new(File::open(filename).unwrap())),
+    };
+    if let Err(err) = io::copy(&mut r, &mut w) {
+        if err.kind() != io::ErrorKind::BrokenPipe {
+            panic!("{}", err);
+        }
     }
-    println!("Splitter didn't crash!");
-
-    print_string(Encoding::UTF16LE.utf8_reader(HELLO_UTF16LE));
-    print_string(Encoding::UTF32BE.utf8_reader(HELLO_UTF32BE));
-
-    print_bytes_and_string(Encoding::UTF16LE.utf8_reader(HELLO_UTF16LE));
-    print_bytes_and_string(Encoding::UTF32BE.utf8_reader(HELLO_UTF32BE));
 }
 
-fn print_string(r: impl Read) {
-    print!("{}", io::read_to_string(r).unwrap());
+#[derive(Parser)]
+struct Cli {
+    #[clap(
+        short,
+        long,
+        name = "encoding",
+        help = "Specifies the encoding of the input",
+        parse(try_from_str = try_parse_encoding),
+    )]
+    from_code: Encoding,
+
+    #[clap(
+        name = "inputfile",
+        help = "A file to read from instead of standard input"
+    )]
+    inputfile: Option<PathBuf>,
 }
 
-fn print_bytes_and_string(r: impl Read) {
-    print!(
-        "{}",
-        String::from_utf8(
-            r.bytes()
-                .map(|b| b.unwrap())
-                .inspect(|b| print!("{:?} ", b))
-                .collect(),
-        )
-        .unwrap()
-    );
+fn try_parse_encoding(arg: &str) -> Result<Encoding, String> {
+    let arg = arg.to_lowercase();
+    let endianness = match &arg {
+        arg if arg.contains("be") => Endianness::Big,
+        arg if arg.contains("le") => Endianness::Little,
+        _ => return Err(format!("can't determine endianness of '{arg}'")),
+    };
+    Ok(match &arg {
+        arg if arg.contains("16") => match endianness {
+            Endianness::Big => Encoding::UTF16BE,
+            Endianness::Little => Encoding::UTF16LE,
+        },
+        arg if arg.contains("32") => match endianness {
+            Endianness::Big => Encoding::UTF32BE,
+            Endianness::Little => Encoding::UTF32LE,
+        },
+        _ => return Err(format!("can't determine bitness of '{arg}'")),
+    })
 }
